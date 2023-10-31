1363627d266deee43b312b9223fe117583b8c04b
Nick Bauters <nick.bauters@continuum.be>
2023-10-31 12:29:55 +0100

    feat: Adds the main menu and its options

--- src/be/continuum/bigbank/menu/BankMainMenu.java
+++ src/be/continuum/bigbank/menu/BankMainMenu.java
@@ -10,5 +10,37 @@ public class BankMainMenu {
         menu.showMenuTitle("Welcome to the Big Bank App");
 
         BankAccount loggedInUser = loginMenu.login();
+
+        mainMenu(loggedInUser);
+    }
+
+    private void mainMenu(BankAccount user) {
+        while(true) {
+            menu.showSubMenuTitle("Main Menu");
+
+            menu.showMenuBlock(
+                    "1. Incoming Transactions",
+                    "2. Outgoing Transactions",
+                    "3. Make Transfer",
+                    "4. Logout"
+            );
+            String choice = menu.askForUserInput("Please enter your choice from the menu: ");
+
+            switch (choice) {
+                case "1":
+                    menu.showMenuItem("HERE BE TRANSACTIONS");
+                    break;
+                case "2":
+                    menu.showMenuItem("HERE BE OTHER TRANSACTIONS");
+                    break;
+                case "3":
+                    menu.showMenuItem("HERE BE TRANSFERS");
+                    break;
+                default:
+                    menu.showError("Fraude detected, Invalid choice!");
+                case "4":
+                    loginMenu.logOut();
+            }
+        }
     }
 }
