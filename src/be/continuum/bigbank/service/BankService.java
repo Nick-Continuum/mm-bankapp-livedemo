f6f881fa581b443c7ae94019ba7c264a4ca3413c
Nick Bauters <nick.bauters@continuum.be>
2023-10-31 12:59:50 +0100

    feat: Adds the usecase logic for a transfer of funds between accounts

---
+++ src/be/continuum/bigbank/service/BankService.java
@@ -0,0 +1,24 @@
+package be.continuum.bigbank.service;
+
+import be.continuum.bigbank.BankAccountInMemoryRepository;
+import be.continuum.bigbank.model.BankAccount;
+import be.continuum.bigbank.model.Transaction;
+
+public class BankService {
+    private final BankAccountInMemoryRepository accountRepository;
+
+    public BankService(BankAccountInMemoryRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public void transfer(Transaction transaction) {
+        BankAccount sender = transaction.getSender();
+        BankAccount receiver = transaction.getReceiver();
+        double amount = transaction.getAmount();
+
+        sender.withdraw(amount);
+        sender.addOutgoingTransaction(transaction);
+        receiver.deposit(amount);
+        receiver.addIncomingTransaction(transaction);
+    }
+}
