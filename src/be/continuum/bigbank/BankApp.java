872c74aa9695d8ba7940340d1c3bc56ddeb005fa
Nick Bauters <nick.bauters@continuum.be>
2023-10-31 10:26:51 +0100

    feat: Adds the main welcome message to the application

--- src/be/continuum/bigbank/BankApp.java
+++ src/be/continuum/bigbank/BankApp.java
@@ -1,7 +1,10 @@
 package be.continuum.bigbank;
 
+import be.continuum.bigbank.menu.BankMainMenu;
+
 public class BankApp {
     public static void main(String[] args) {
-
+        BankMainMenu mainMenu = new BankMainMenu();
+        mainMenu.run();
     }
 }
---
+++ src/be/continuum/bigbank/menu/BankMainMenu.java
@@ -0,0 +1,11 @@
+package be.continuum.bigbank.menu;
+
+public class BankMainMenu {
+    public void run() {
+        System.out.println("+++++      +++++++       ++++++");
+        System.out.println("+ Welcome to the Big Bank App +");
+        System.out.println("+++++      +++++++       ++++++");
+
+
+    }
+}
