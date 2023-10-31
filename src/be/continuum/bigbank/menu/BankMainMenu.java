2047eb9a57e32eae216b0b9cfe8eaeaeff3b4d8f
Nick Bauters <nick.bauters@continuum.be>
2023-10-31 10:37:07 +0100

    feat: Implements the User interaction for getting the IBAn and Pincode of a user

--- src/be/continuum/bigbank/menu/BankMainMenu.java
+++ src/be/continuum/bigbank/menu/BankMainMenu.java
@@ -1,11 +1,18 @@
 package be.continuum.bigbank.menu;
 
+import java.util.Scanner;
+
 public class BankMainMenu {
     public void run() {
         System.out.println("+++++      +++++++       ++++++");
         System.out.println("+ Welcome to the Big Bank App +");
         System.out.println("+++++      +++++++       ++++++");
 
-
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your IBAN: ");
+        String authenticatingUserIban = keyboard.next();
+        System.out.println("Please enter your Pincode: ");
+        String authenticatingUserPin = keyboard.next();
+        int userPin = Integer.parseInt(authenticatingUserPin);
     }
 }
